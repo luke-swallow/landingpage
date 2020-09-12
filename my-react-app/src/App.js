a2d2dae023a7c653aa0336eaa1c44b955f8e9702
Charles Wang <wcharles168gmail.com>
2020-09-12 00:21:06 -0400

    bare bones landing page

---
+++ my-react-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {NavbarComponent} from './components/Navbar';
+import {SectionComponent} from './components/Section';
+
+function App() {
+  return (
+    <div>
+      <NavbarComponent/>
+      <SectionComponent/>
+    </div>
+  );
+}
+
+export default App;
